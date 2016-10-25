d1a7513aeb512f816de3cf4b6f21e6db57a250c9
川崎隆介 <kawasaki@kawasakiryuusuke-no-MacBook-Pro.local>
2016-10-25 13:06:19 +0900

    Initial Commit

---
+++ ConsoleSample/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ConsoleSample
+//
+//  Created by 川崎隆介 on 2016/10/25.
+//  Copyright © 2016年 codable. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
